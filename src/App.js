d014729ea5e2a7d31ecd785fed7ee199724ce596
hafiz1993 <hafizurrahaman04@gmail.com>
2020-08-25 01:29:41 +0600

    random user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(()=>{
+    fetch('https://randomuser.me/api/')
+    .then (res=> res.json())
+    .then (data => setUsers(data.results))
+  }, [])
+
+  // const users = [{name :'Dipjol', age: 30, country:'Bangladesh'}, {name :'Dev', age: 30, country:'india'}, {name :'Jit', age: 30, country:'varot'}];
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        
+        {/* <MovieCounter></MovieCounter> */}
+
+        {
+          users.map(user => <Users 
+            profile ={user.picture.large}
+            title = {user.name.title}
+            fname = {user.name.first}
+            lname = {user.name.last} 
+            email ={user.email}
+            age = {user.dob.age}
+            phone ={user.phone}
+            info = {user.id.name}
+            
+            >
+              
+            </Users>)
+          
+        }
+  
       </header>
+      
     </div>
   );
+}
+
+function MovieCounter(){
+  const [count, setCount]= useState(0);
+  
+const handleClick = () => setCount(count + 1);
+
+
+  return (
+  <div>
+    <button onClick ={handleClick}>Add Movies</button>
+    
+  <h5>Numbers of Movies :- {count} </h5>
+  <MovieDisplay movies ={count+10}></MovieDisplay>
+  <MovieDisplay movies ={count+20}></MovieDisplay>
+  <MovieDisplay movies ={count+30}></MovieDisplay>
+  </div>
+  )
+}
+
+function MovieDisplay(props){
+return <h4>Movies I have Seen :- {props.movies}</h4>
+}
+
+function Users(props){
+ 
+  
+  const userStyle = {
+    border: '2px solid red',
+    width: '800px',
+    borderRadius: '20px',
+    margin:'10px'  
+    
+
+  }
+  const imageStyle ={
+    borderRadius: '50%'
+  }
+  return (
+    <div style={userStyle}>
+      <img style={imageStyle} src={props.profile} alt=""/>
+        <h3>Name: {props.title} {props.fname} {props.lname} </h3>
+        <p>Email: {props.email}</p>
+        <p>Age: {props.age}</p>
+        <p>Phone: {props.phone}</p>
+  <p>Information: {props.info}</p>
+        
+        
+    
+    
+  
+    </div>
+    )
+    
 }
 
 export default App;
